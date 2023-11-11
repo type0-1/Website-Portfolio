f7f3196428751f7710b3ab6a50b1609db6f9ac76
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-11-11 15:23:02 +0000

    Experimentation with JS file, removed previous typewriter effect due to overlap.

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -16,22 +16,3 @@ window.addEventListener("scroll", function () {
         }
     });
 });
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('nav a');
-
-    links.forEach(link => {
-        link.addEventListener('click', smoothScroll);
-    });
-
-    function smoothScroll(e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetElement.offsetTop - document.querySelector('.header').offsetHeight,
-            behavior: 'smooth'
-        });
-    }
-});
